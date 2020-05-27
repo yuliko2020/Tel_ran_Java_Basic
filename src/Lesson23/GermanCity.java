7dfbcf7489c42078a80de6bb51563d0239295985
yuliko2020 <yuliiascicluna@gmail.com>
2020-05-27 21:33:09 +0200

    Lesson23 additions

--- src/Lesson23/GermanCity.java
+++ src/Lesson23/GermanCity.java
@@ -4,5 +4,19 @@ package Lesson23;
  * TelRanJavaBasicModulOne
  * 27/05/2020  17:30
  */
-public class GermaCity {
+public class GermanCity {
+    public static void main(String[] args) {
+        germanCity();
+    }
+
+    public static void germanCity() {
+        String[] germanCity = new String[]{"Berlin", "München", "Dresden", "Hamburg", "Köln", "Potsdam"};
+        int allGermanCity = germanCity.length;
+        int allLetter = 0;
+        for (int i = 0; i < allGermanCity - 1; i++) {
+            allLetter = allLetter + germanCity[i].length();
+        }
+        System.out.println("allGermanCity: " + allGermanCity);
+        System.out.println("allLetter: " + allLetter);
+    }
 }
--- src/Lesson23/HomeWorkArray23.java
+++ src/Lesson23/HomeWorkArray23.java
@@ -7,19 +7,22 @@ package Lesson23;
 public class HomeWorkArray23 {
     public static void main(String[] args) {
         myFirstArrayExercise(5);
-    }
 
+    }
     public static void myFirstArrayExercise(int elementCounts) {
         int[] digits = new int[elementCounts];
 
         for (int element : digits) {
             element = (int) (Math.random() * ((100 - 1) + 1));
-            System.out.println("--------");
         }
+        System.out.println("-------------");
+
         for (int i = 0; i < elementCounts; i++) {
             digits[i] = (int) (Math.random() * ((100 - 1) + 1));
-            System.out.println("-------");
+            System.out.println(digits[i]);
         }
+        System.out.println("-------------");
+
         int min = digits[0];
         int max = digits[0];
         int middle = 0;
@@ -27,9 +30,24 @@ public class HomeWorkArray23 {
         for (int i = 0; i < elementCounts; i++) {
             if (digits[i] < min) {
                 min = digits[i];
-
             }
-            System.out.println(min);
         }
+
+        for (int i = 0; i < elementCounts; i++) {
+            if (digits[i] > max) {
+                max = digits[i];
+            }
+        }
+
+        for (int i = 0; i < elementCounts; i++) {
+            middle = middle + digits[i];
+            System.out.println(middle);
+        }
+        middle = middle / elementCounts;
+        System.out.println("Среднее арифметическое значение элементов: " + middle);
+        System.out.println("Максимальное значение найдено: " + max);
+        System.out.println("Найдено минимальное значение массива: " + min);
     }
+
+
 }
